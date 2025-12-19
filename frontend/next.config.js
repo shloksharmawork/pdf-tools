7d5005217279b4dfdfeeeeb10b58a904e5147119
shlokfreelancer <shlokporfessionalwork@gmail.com>
2025-12-19 21:48:43 +0530

    Remove rewrites configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,18 +7,6 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
-    async rewrites() {
-        const backendUrl = process.env.BACKEND_URL
-            ? `https://${process.env.BACKEND_URL}`
-            : 'http://127.0.0.1:8000';
-
-        return [
-            {
-                source: '/api/:path*',
-                destination: `${backendUrl}/api/:path*`,
-            },
-        ]
-    },
 }
 
 module.exports = nextConfig
